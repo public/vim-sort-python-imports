c097199f7e3f34f800811ef17d92a8b6be9e957b
Alex Stapleton <alex.stapleton@greenmangaming.com>
2013-02-04 07:55:49 +0000

    Kind of sort of working import tidier.

---
+++ test.py
@@ -0,0 +1,34 @@
+
+import os.path.abspath
+import os.path
+import os
+import os as os2
+
+import os, sys
+import os as os3, sys as sys2
+
+import S,R,Q
+
+import os,os,os,os
+
+from os import path
+from os import path as ospath
+
+from os.path import (abspath, realpath)
+
+from os.path import abspath
+from os.path import abspath as abspath2
+
+from W import (ABC, DEF, HIJ, LMK, NOP, QRST, ODKOK, POKDSPFOKSDF, POKSDFOPKSDF,
+               POKDPOK, LKLKJDSF, SDFDSFSDF, iIJIJIJ
+               )
+
+def F():
+    import LIB
+
+from X import C, B, A
+
+from . import a
+from .. import b
+from ... import c
+
